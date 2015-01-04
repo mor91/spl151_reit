de2c756bf01f77cbec4e34b7207690cb6d9351db
chen <chen@chen-Inspiron-N5110>
2015-01-04 22:09:22 +0200

    jjunit3

--- test/WarehouseTest.java
+++ test/WarehouseTest.java
@@ -1,82 +1,89 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-///**
-// *
-// * @author chen
-// */
-//public class WarehouseTest {
-//    
-//    public WarehouseTest() {
-//    }
-//    
-//    @BeforeClass
-//    public static void setUpClass() {
-//    }
-//    
-//    @AfterClass
-//    public static void tearDownClass() {
-//    }
-//    
-//    @Before
-//    public void setUp() {
-//    }
-//    
-//    @After
-//    public void tearDown() {
-//    }
-//
-//    /**
-//     * Test of acquireTool method, of class Warehouse.
-//     */
-//    @Test
-//    public void testAcquireTool() {
-//        System.out.println("acquireTool");
-//        String toolName = "hammer";
-//        Warehouse instance = new Warehouse();
-//        
-//        //int toolCount=instance.getToolCount(toolName);
-//        instance.acquireTool(toolName);
-//        //assertEquals(toolCount-1, instance.getToolCount(toolName));
-//    }
-//
-//    /**
-//     * Test of releaseTool method, of class Warehouse.
-//     */
-//    @Test
-//    public void testReleaseTool() {
-//        System.out.println("releaseTool");
-//        String toolName = "hammer";
-//        Warehouse instance = new Warehouse();
-//        
-//        //int toolCount=instance.getToolCount(toolName);
-//        instance.releaseTool(toolName);
-//        //assertEquals(toolCount+1, instance.getToolCount(toolName));
-//
-//    }
-//
-//    /**
-//     * Test of consumeMaterial method, of class Warehouse.
-//     */
-//    @Test
-//    public void testConsumeMaterial() {
-//        System.out.println("consumeMaterial");
-//        String materialName = "concrete";
-//        Warehouse instance = new Warehouse();
-//        
-//        //int materialCount=instance.getMaterialCount(materialName);
-//        instance.consumeMaterial(materialName);
-//        //assertTrue(materialCount<instance.getMaterialCount(materialName));
-//    }
-//
-//    
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author chen
+ */
+public class WarehouseTest extends TestCase {
+    
+    public WarehouseTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of acquireTool method, of class Warehouse.
+     */
+    public void testAcquireTool() {
+        System.out.println("acquireTool");
+        String toolName = "";
+        Warehouse instance = new Warehouse();
+        instance.acquireTool(toolName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of releaseTool method, of class Warehouse.
+     */
+    public void testReleaseTool() {
+        System.out.println("releaseTool");
+        String toolName = "";
+        Warehouse instance = new Warehouse();
+        instance.releaseTool(toolName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of consumeMaterial method, of class Warehouse.
+     */
+    public void testConsumeMaterial() {
+        System.out.println("consumeMaterial");
+        String materialName = "";
+        Warehouse instance = new Warehouse();
+        instance.consumeMaterial(materialName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addRepairTool method, of class Warehouse.
+     */
+    public void testAddRepairTool() {
+        System.out.println("addRepairTool");
+        RepairTool repairTool = null;
+        Warehouse instance = new Warehouse();
+        instance.addRepairTool(repairTool);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addRepairMaterial method, of class Warehouse.
+     */
+    public void testAddRepairMaterial() {
+        System.out.println("addRepairMaterial");
+        RepairMaterial repairMaterial = null;
+        Warehouse instance = new Warehouse();
+        instance.addRepairMaterial(repairMaterial);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
